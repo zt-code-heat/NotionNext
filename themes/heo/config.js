add78b06e377dad9f5527f13c9077a804780268d
zt-code-heat <lyqbeztcha@gmail.com>
2026-05-20 16:37:13 +0800

    Update hero section configuration in config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -23,7 +23,7 @@ const CONFIG = {
   //HEO_HERO_TITLE_3: 'TANGLY1024.COM',
   //HEO_HERO_TITLE_4: '新版上线',
   //HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  //HEO_HERO_TITLE_LINK: 'https://temp.aoki.dpdns.org',
+  HEO_HERO_TITLE_LINK: 'https://notion-next-19dt.top/',
   // 英雄区遮罩文字
   //HEO_HERO_COVER_TITLE: '随便逛逛',
 
@@ -35,11 +35,12 @@ const CONFIG = {
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多一篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
   HEO_HERO_RECOMMEND_POST_COUNT: 1, // 只显示1篇推荐
+  HEO_HERO_RECOMMEND_COVER_SCALE: 1.4, // 放大图片
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
-  //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
+     HERO_RECOMMEND_COVER: '8015c16f210710fd996da7dec3d9a0a7.jpg', // 英雄区右侧图片
   
   // 英雄区右侧推荐文章遮罩控制
-  HEO_HERO_RECOMMEND_COVER_ENABLE: false, // 是否显示推荐文章遮罩图片，true显示遮罩需点击查看，false直接显示推荐文章
+  HEO_HERO_RECOMMEND_COVER_ENABLE: true, // 是否显示推荐文章遮罩图片，true显示遮罩需点击查看，false直接显示推荐文章
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
