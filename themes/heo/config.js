d7b1ad2c60c635b80f7260cc5002749b03918272
zt-code-heat <lyqbeztcha@gmail.com>
2026-05-22 14:33:50 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -44,13 +44,13 @@ const CONFIG = {
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   //HEO_INFOCARD_GREETINGS: [
-    '你好！我是',
-    '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
-    '🏃 脚踏实地行动派',
-    '🏠 智能家居小能手',
-    '🤖️ 数码科技爱好者',
-    '🧱 团队小组发动机'
+  //  '你好！我是',
+    //'🔍 分享与热心帮助',
+    //'🤝 专修交互与设计',
+    //'🏃 脚踏实地行动派',
+    //'🏠 智能家居小能手',
+    //'🤖️ 数码科技爱好者',
+    //'🧱 团队小组发动机'
   ],
 
   // 个人资料底部按钮
@@ -59,7 +59,7 @@ const CONFIG = {
   HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
   HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
-  HEO_INFO_CARD_TEXT3: '了解更多',
+  //HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
