41a50b003f6bd279d396a6428b02a26ac8dd6560
zt-code-heat <lyqbeztcha@gmail.com>
2026-05-23 16:46:33 +0800

    Update index.js

--- themes/heo/index.js
+++ themes/heo/index.js
@@ -75,8 +75,7 @@ const LayoutBase = props => {
   )
 
   // 右侧栏 用户信息+标签列表
-  const slotRight =
-    router.route === '/404' || fullWidth ? null : <SideRight {...props} />
+  const slotRight = null
 
   const maxWidth = fullWidth ? 'max-w-[96rem] mx-auto' : 'max-w-[86rem]' // 普通最大宽度是86rem和顶部菜单栏对齐，留空则与窗口对齐
 
